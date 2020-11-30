121bade4f848058e8193ef7a1d0039f9b7a68b78
alex <alexander.topilskii@gmail.com>
2020-11-30 21:59:21 +0300

    aa

--- src/Application.kt
+++ src/Application.kt
@@ -5,7 +5,9 @@ import io.ktor.response.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.http.ContentType
+import io.ktor.request.receiveParameters
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -18,7 +20,16 @@ fun Application.module(testing: Boolean = false) {
     val client = HttpClient(Apache) {
         routing {
             get("/kek") {
-                call.respondText("Hello World!2", ContentType.Text.Plain)
+                call.respondText("Hello Local World!", ContentType.Text.Plain)
+            }
+
+            post("/test2") {
+                val parameters = call.receiveParameters()
+
+                val paramVal1 = parameters["param1"]
+                val paramVal2 = parameters["param2"]
+
+                call.respondText("This is a test POST request with parameter values $paramVal1 and $paramVal2 and params: $parameters")
             }
         }
     }
